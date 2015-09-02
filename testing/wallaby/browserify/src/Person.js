e50a3488b733666587b4bcac556d203a5e2a0b74
deeperx <inanc@pixenka.com>
2015-09-02 14:24:38 +0300

    fix: wallaby browserify tests

--- testing/wallaby/browserify/src/Person.js
+++ testing/wallaby/browserify/src/Person.js
@@ -1,5 +1,10 @@
-var Person = function (name) {
+var Person = function (name, surname) {
   this.name = name;
+  this.surname = surname;
+
+  this.fullname = function() {
+    return (this.name +' '+ (this.surname || '')).trim();
+  }
 };
 
 module.exports = Person;

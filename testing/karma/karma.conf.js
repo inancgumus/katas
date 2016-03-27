2dd01beff046eef1ecf877131a5d589ea1fe4a02
deeperx <inanc@pixenka.com>
2016-03-27 20:43:32 +0300

    add: basic karma test runner config

---
+++ testing/karma/karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    files: [
+      'src/**/*.js',
+      'test/**/*.spec.js'
+    ],
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-chrome-launcher',
+      'karma-mocha',
+      'karma-mocha-reporter',
+      'karma-chai',
+      'karma-coverage'
+    ],
+    preprocessors: {
+      'src/**/*.js': ['coverage']
+    },
+    browsers: ['PhantomJS', 'Chrome'],
+    frameworks: ['mocha', 'chai'],
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    },
+
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+    colors: true,
+    port: 9876,
+    concurrency: Infinity
+  })
+}

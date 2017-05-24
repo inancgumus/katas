711a55dd63539ceda034de7956ff26959bdee1c3
Inanc Gumus <inanc@pixenka.com>
2017-05-24 21:49:24 +0300

    add: fanint go channels example

---
+++ go/fanin.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func producer(name string, in chan int, d time.Duration) {
+    var i int
+    for {
+        fmt.Println("--->", name, "produced:", i)
+        in <- i
+        i++
+        time.Sleep(d)
+    }
+}
+
+func consumer(out chan int) {
+    for x := range out {
+        fmt.Println("consumed:", x)
+				time.Sleep(5000 * time.Millisecond)
+    }
+}
+
+func main() {
+    in := make(chan int)
+    out := make(chan int)
+    go producer("producer 1", in, 1*time.Second)
+    go producer("producer 2", in, 2*time.Second)
+    go consumer(out)
+
+    for i := range in {
+        out <- i
+    }
+}

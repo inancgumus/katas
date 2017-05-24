35af625ee879e66a542e14a929be51de6821c723
Inanc Gumus <inanc@pixenka.com>
2017-05-24 21:21:04 +0300

    add: simpler goroutine timer

---
+++ go/timer_simpler.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func timer(d time.Duration, i int) <-chan int {
+    c := make(chan int)
+    go func() {
+        time.Sleep(d)
+        c <- i
+    }()
+    return c
+}
+
+func main() {
+    for i := 0; i < 5; i++ {
+        c := timer(100 * time.Millisecond, i + 1)
+        fmt.Println(<-c)
+    }
+}

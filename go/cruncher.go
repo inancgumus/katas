bd256e7eb9e74f946bb7eb0321d7163f7290739c
Inanc Gumus <inanc@pixenka.com>
2017-05-25 11:41:58 +0300

    add: go channel number cruncher example

---
+++ go/cruncher.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+type cruncher func(int) int
+
+func sq(in <-chan int, f cruncher) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- f(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func printer(in <-chan int) {
+	for n := range in {
+		fmt.Println(n)
+	}
+}
+
+func main() {
+	sqf := func(n int) int {
+		return n * n
+	}
+
+	sqrf := func(n int) int {
+		return int(math.Sqrt(float64(n)))
+	}
+
+	printer(sq(sq(gen(1, 2, 3, 4, 5), sqf), sqrf))
+}

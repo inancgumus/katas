377cf95f4444f8f63a16398ec9bd777f50edefcc
Inanc Gumus <inanc@pixenka.com>
2017-05-25 12:06:09 +0300

    fix: formatting on go examples

--- go/memtestbyref.go
+++ go/memtestbyref.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func main() {
-  var mils [1e7]int
+	var mils [1e7]int
 
-  func() {
-    // good: copying arrays by passing by reference
-    var passp = func(n *[1e7]int) *[1e7]int { return n }
-    passp(&mils)
-  }()
-  fmt.Printf("should sleep...")
-  time.Sleep(time.Millisecond * 5000)
+	func() {
+		// good: copying arrays by passing by reference
+		var passp = func(n *[1e7]int) *[1e7]int { return n }
+		passp(&mils)
+	}()
+	fmt.Printf("should sleep...")
+	time.Sleep(time.Millisecond * 5000)
 
-  func() {
-    // bad: copying arrays by passing by value
-    var pass = func(n [1e7]int) [1e7]int { return n }
-    pass(mils)
-  }()
-  fmt.Printf("should sleep...")
-  time.Sleep(time.Millisecond * 5000)
+	func() {
+		// bad: copying arrays by passing by value
+		var pass = func(n [1e7]int) [1e7]int { return n }
+		pass(mils)
+	}()
+	fmt.Printf("should sleep...")
+	time.Sleep(time.Millisecond * 5000)
 }
 
 func p(format string, a ...interface{}) {
 	fmt.Printf("* "+format+"\n", a...)
 }
-
